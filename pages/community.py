643725c09415863d5c9a47e0385699e9b2dcfa0f
Jaime <jaimemarijke@gmail.com>
2020-03-12 16:04:06 -0700

    Task #3: use SQL

--- pages/community.py
+++ pages/community.py
@@ -2,18 +2,19 @@ from flask import jsonify, render_template, request, Response
 from flask.ext.login import current_user, login_user
 
 from functions import app
-from models import User
-from sqlalchemy import desc
+from models import User, db
 
+
+RECENT_USER_COUNT = 5
+RECENT_USER_QUERY = 'SELECT * FROM user ORDER BY signup_date DESC LIMIT {};'.format(RECENT_USER_COUNT)
 
 @app.route('/community', methods=['GET'])
 def community():
 
     login_user(User.query.get(1))
 
-    # TODO: the instructions say to use raw SQL instead of an ORM
-    # I was getting hung up on getting a direct connection to the database to be able to execute raw SQL
-    recent_users = User.query.order_by(desc(User.signup_date)).limit(5).all()
+    connection = db.session.connection()
+    recent_users = connection.execute(RECENT_USER_QUERY)
 
     args = {
             'recent_users': [
